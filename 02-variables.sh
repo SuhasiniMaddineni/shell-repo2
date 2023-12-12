1a19f52a9730714f83742526320ba740216d6730
SuhasiniMaddineni <suhasini.maddineni90@gmail.com>
2023-12-12 23:11:12 +0530

    comited in repo

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,5 +3,5 @@
 person1=maanas
 person2=suresh
 
-echo "$person1: hi, good mrng $person2"
-echo "$person2: hi, very good mrng $person1"
+echo "$person1: hi good mrng $person2"
+echo "$person2: hi very good mrng $person1"
