c57fc7e0623d4e13033d1316181530aee2ac82fe
SuhasiniMaddineni <suhasini.maddineni90@gmail.com>
2023-12-16 00:02:55 +0530

    commit all

--- 05-variable.sh
+++ 05-variable.sh
@@ -8,6 +8,9 @@ sum=$(($number1+$number2))
  echo "sum is: ${sum}"
 
  echo "all arguments: $@"
- ech "no.of arguments: $#"
+ ech0 "how many arguments passed: $#"
  echo "script name: $0"
- 
+ echo "first argu: $1"
+ echo "2nd argu: $2"
+ echo "nth argu: $N"
+
