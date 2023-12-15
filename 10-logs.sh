07787dc2b475cf89cd4a4b691262521f9e738b60
SuhasiniMaddineni <suhasini.maddineni90@gmail.com>
2023-12-16 00:46:06 +0530

    commit all

--- 10-logs.sh
+++ 10-logs.sh
@@ -11,12 +11,17 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 #echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
 ID=$(id -u)
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+O="\e[0m"
+
 if [ $ID -ne 0 ]
     then
-       echo "error: not root user"
+       echo "$R error: not root user"
        exit 1
     else
-       echo "inside root user"
+       echo "$G inside root user"
 fi
 echo "script name is: $0"
 echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
@@ -25,10 +30,10 @@ validate(){
 
     if [ $1 -ne 0 ]
     then
-      echo "error: $2.... failed"
+      echo "$R $2.... failed"
       exit 1
     else
-      echo "$2.... success"
+      echo "$G $2.... success"
     fi
 }
 
