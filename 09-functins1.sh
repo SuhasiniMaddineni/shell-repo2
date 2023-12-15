c9abb74ae1349036e9d6630a987e35608204b31a
SuhasiniMaddineni <suhasini.maddineni90@gmail.com>
2023-12-15 23:22:25 +0530

    commit all

--- 09-functins1.sh
+++ 09-functins1.sh
@@ -10,19 +10,19 @@ if [ $ID -ne 0 ]
        echo "inside root user"
 fi
 
-yum install mysql -y
-validate
-yum install git -y
-validate
-
 validate(){
 
     if [ $? -ne 0 ]
     then
-    echo "error: installed failed"
-    exit 1
+      echo "error: installed failed"
+      exit 1
     else
-    echo "installed success"
+      echo "installed success"
     fi
 }
+
+yum install mysql -y
+validate
+yum install git -y
+validate
 
