f277c2c5d3a22d45443a9e047ea20fd643426c76
SuhasiniMaddineni <suhasini.maddineni90@gmail.com>
2023-12-16 12:47:41 +0530

    loop cmmited

--- 12-install-packages.sh
+++ 12-install-packages.sh
@@ -22,11 +22,11 @@ fi
 
 validate(){
   if [ $1 -ne 0 ]
-  then
-  echo -e "error:$2... $R failed $N"
-  else
-  echo -e "$2...$G success $N"
-
+    then
+    echo -e "error:$2... $R failed $N"
+    else
+    echo -e "$2...$G success $N"
+  fi
 }
 
 
