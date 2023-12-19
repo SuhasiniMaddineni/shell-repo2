f231ee72984f075fdfd64faf9928b7ebf46496e4
SuhasiniMaddineni <suhasini.maddineni90@gmail.com>
2023-12-19 10:11:43 +0530

    disk-usage

--- 16-disk-usage.sh
+++ 16-disk-usage.sh
@@ -6,12 +6,11 @@ message=""
 
 while IFS=read line
 do
- usage=$(echo $line | awk '{print $6F}' | cut -d % -f1) 
- partition=$(echo $line | awk '{print $1F}')
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1) 
+    partition=$(echo $line | awk '{print $1F}')
+    
+    if [ $usage -gt $DISK_THRESHOLD]
+    then
+    message+="High disk usage on $partition: $usage"
+    fi
 done <<< $DISK_USAGE 
-
-if [ $usage -gt $DISK_THRESHOLD]
-then
-  message+="High disk usage on $partition: $usage"
-fi
-
