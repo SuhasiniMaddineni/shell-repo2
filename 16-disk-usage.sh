68f02fe2918c2ab3c8caf2bc047b542e67cac9f9
SuhasiniMaddineni <suhasini.maddineni90@gmail.com>
2023-12-19 10:10:42 +0530

    disk-usage

--- 16-disk-usage.sh
+++ 16-disk-usage.sh
@@ -4,14 +4,14 @@ DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
 DISK_THRESHOLD=1 # more then 1% it should be alerted
 message=""
 
-while IFS= read line
+while IFS=read line
 do
  usage=$(echo $line | awk '{print $6F}' | cut -d % -f1) 
  partition=$(echo $line | awk '{print $1F}')
+done <<< $DISK_USAGE 
 
 if [ $usage -gt $DISK_THRESHOLD]
 then
   message+="High disk usage on $partition: $usage"
 fi
 
-done <<< $DISK_USAGE
